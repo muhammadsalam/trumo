6f1bdd7fda02eef6969f104177be93238921ffa2
Magomedsalam Musaev <muhammadsalam@list.ru>
2022-03-27 10:53:34 +0300

    categories недокончено

---
+++ js/function.js
@@ -0,0 +1,14 @@
+function formToggle() {
+    let form = document.querySelector(".header-form");
+    form.classList.toggle("active");
+}
+
+function toUp() {
+    window.scrollTo(0, 0);
+}
+
+function headerToggle() {
+    let hButton = document.querySelector('.header-btn');
+    hButton.classList.toggle('active');
+}
+
